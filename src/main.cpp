4956996f49d0c5c6c5cd0c2a130f21c9dab57993
diginatu <diginatu.510o@gmail.com>
2015-04-04 19:10:46 +0900

    added command line options

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,18 @@ int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
 
+  QCoreApplication::setApplicationName("Viqo");
+  QCoreApplication::setApplicationVersion("2.0");
+
+  QCommandLineParser parser;
+  parser.setApplicationDescription(
+        QStringLiteral("Qt で作成されたマルチプラットフォームコメビュです"));
+  parser.addHelpOption();
+  parser.addVersionOption();
+
+  // Process the actual command line arguments given by the user
+  parser.process(a);
+
   MainWindow w;
   w.show();
 
