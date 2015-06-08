105f165b8abc1cb10069fced6f99ff7b68dbdbe9
diginatu <diginatu.510o@gmail.com>
2015-06-08 12:38:03 +0900

    SearchNotifyBroad : works for broadID, communityID and userID

--- src/NicoLiveManager/Alert/wakutcp.cpp
+++ src/NicoLiveManager/Alert/wakutcp.cpp
@@ -1,4 +1,4 @@
-#include "wakutcp.h"
+﻿#include "wakutcp.h"
 #include "../../../ui/mainwindow.h"
 #include "../nicolivemanager.h"
 
@@ -88,7 +88,7 @@ void WakuTcp::readOneRawWaku(const QString& rawwaku)
   QStringList wakur = wakuS.split(",");
   const QString broadID = wakur.at(0);
   const QString CommunityID = wakur.at(1);
-  //	const QString& nushiID = wakur.at(2);
+  const QString& nushiID = wakur.at(2);
 
   if(wakur.size() != 3) {
     return;
@@ -96,10 +96,50 @@ void WakuTcp::readOneRawWaku(const QString& rawwaku)
 
   nicolivemanager->allGotWakuInfo(CommunityID, broadID);
 
+  auto insertNewWaku = [&](){
+    nicolivemanager->insertLiveWakuList(new LiveWaku(mwin, nicolivemanager, broadID, CommunityID));
+    mwin->refleshLiveWaku();
+  };
+
   foreach (QString commu, nicolivemanager->mycommunities) {
     if (commu == wakur.at(1)) {
-      nicolivemanager->insertLiveWakuList(new LiveWaku(mwin, nicolivemanager, broadID, CommunityID));
-      mwin->refleshLiveWaku();
+      insertNewWaku();
+    }
+  }
+
+  auto matchRegAndKey = [](bool isReg, const QString& keyword, const QString& matchSt, bool isEq){
+    if (isReg) {
+      if (QRegExp(matchSt).indexIn(keyword) != -1)
+        return true;
+    } else {
+      if (isEq) {
+        if (keyword == matchSt) return true;
+      } else {
+        if (matchSt.indexOf(keyword)!=-1) return true;
+      }
+    }
+    return false;
+  };
+
+  qDebug() << awaku.toString();
+  foreach (QStringList match, mwin->settings.matchDateList) {
+    if (match[1].indexOf('B')!=-1) {
+      if (matchRegAndKey(match[2]=="true", match[3], broadID, true)) {
+        insertNewWaku();
+        break;
+      }
+    }
+    if (match[1].indexOf('C')!=-1) {
+      if (matchRegAndKey(match[2]=="true", match[3], CommunityID, true)) {
+        insertNewWaku();
+        break;
+      }
+    }
+    if (match[1].indexOf('U')!=-1) {
+      if (matchRegAndKey(match[2]=="true", match[3], nushiID, true)) {
+        insertNewWaku();
+        break;
+      }
     }
   }
 
