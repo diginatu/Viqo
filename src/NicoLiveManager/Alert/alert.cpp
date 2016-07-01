f0e9d3acad48203a52e4e736377f1b490715388b
diginatu <diginatu.510o@gmail.com>
2016-07-01 20:16:09 +0900

    fix bug: crash after launching
    If the network connection is not available, it will crash after launching.

--- src/NicoLiveManager/Alert/alert.cpp
+++ src/NicoLiveManager/Alert/alert.cpp
@@ -3,130 +3,137 @@
 
 void NicoLiveManager::loginAlertAPI(const QString& mail, const QString& pass)
 {
-  if(mLoginAlertManager!=nullptr)  delete mLoginAlertManager;
-  mLoginAlertManager = new QNetworkAccessManager(this);
+    if(mLoginAlertManager!=nullptr)  delete mLoginAlertManager;
+    mLoginAlertManager = new QNetworkAccessManager(this);
 
-  connect(mLoginAlertManager, SIGNAL(finished(QNetworkReply*)),
-          this, SLOT(loginAlertFinished(QNetworkReply*)));
+    connect(mLoginAlertManager, SIGNAL(finished(QNetworkReply*)),
+            this, SLOT(loginAlertFinished(QNetworkReply*)));
 
-  QNetworkRequest rq(QUrl("https://secure.nicovideo.jp/secure/login?site=nicolive_antenna"));
-  rq.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+    QNetworkRequest rq(QUrl("https://secure.nicovideo.jp/secure/login?site=nicolive_antenna"));
+    rq.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
 
-  QUrlQuery params;
-  params.addQueryItem("mail", QUrl::toPercentEncoding(mail));
-  params.addQueryItem("password", QUrl::toPercentEncoding(pass));
+    QUrlQuery params;
+    params.addQueryItem("mail", QUrl::toPercentEncoding(mail));
+    params.addQueryItem("password", QUrl::toPercentEncoding(pass));
 
-  mLoginAlertManager->post(rq, params.toString(QUrl::FullyEncoded).toUtf8());
+    mLoginAlertManager->post(rq, params.toString(QUrl::FullyEncoded).toUtf8());
 }
 
 void NicoLiveManager::loginAlertFinished(QNetworkReply* reply)
 {
-  mwin->insLog("NicoLiveManager::loginAlertFinished");
-  QString repdata = QString(reply->readAll());
+    mwin->insLog("NicoLiveManager::loginAlertFinished");
+    QString repdata = QString(reply->readAll());
 
-  StrAbstractor commTcpi(repdata);
+    StrAbstractor commTcpi(repdata);
 
-  QString status = commTcpi.midStr("status=\"","\"");
-  if (status == "fail") {
-    const QString code = commTcpi.midStr("<code>","</code>");
-    const QString description = commTcpi.midStr("<description>","</description>");
-    mwin->insLog(code + "\n" + description + "\n");
-    QMessageBox::information(mwin, "Viqo",
-                             QStringLiteral(u"放送開始通知のログイン(LoginAlert)に失敗しました\n\
-                             メールアドレスとパスワードを確認してください\n\
-                             code : ") + code + "\n" + description);
-    return;
-  }
+    QString status = commTcpi.midStr("status=\"","\"");
+    if (status == "fail") {
+        const QString code = commTcpi.midStr("<code>","</code>");
+        const QString description = commTcpi.midStr("<description>","</description>");
+        mwin->insLog(code + "\n" + description + "\n");
+        QMessageBox::information(mwin, "Viqo",
+                                 QStringLiteral(u"放送開始通知のログイン(LoginAlert)に失敗しました\n\
+                                                メールアドレスとパスワードを確認してください\n\
+                                                code : ") + code + "\n" + description);
+                                                return;
+    }
 
-  QString ticket = commTcpi.midStr("<ticket>","</ticket>");
+    QString ticket = commTcpi.midStr("<ticket>","</ticket>");
 
-  adminAlertAPI(ticket);
-  mwin->insLog();
-  reply->deleteLater();
+    adminAlertAPI(ticket);
+    mwin->insLog();
+    reply->deleteLater();
 }
 
 void NicoLiveManager::adminAlertAPI(const QString& ticket)
 {
-  if(mAdminAlertManager!=nullptr)  delete mAdminAlertManager;
-  mAdminAlertManager = new QNetworkAccessManager(this);
+    if(mAdminAlertManager!=nullptr)  delete mAdminAlertManager;
+    mAdminAlertManager = new QNetworkAccessManager(this);
 
-  connect(mAdminAlertManager, SIGNAL(finished(QNetworkReply*)),
-          this, SLOT(adminAlertFinished(QNetworkReply*)));
+    connect(mAdminAlertManager, SIGNAL(finished(QNetworkReply*)),
+            this, SLOT(adminAlertFinished(QNetworkReply*)));
 
-  QByteArray data;
-  data.append("ticket=" + ticket);
+    QByteArray data;
+    data.append("ticket=" + ticket);
 
-  QNetworkRequest rq(QUrl("http://live.nicovideo.jp/api/getalertstatus"));
-  rq.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+    QNetworkRequest rq(QUrl("http://live.nicovideo.jp/api/getalertstatus"));
+    rq.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
 
-  mAdminAlertManager->post(rq, data);
+    mAdminAlertManager->post(rq, data);
 }
 
 void NicoLiveManager::adminAlertFinished(QNetworkReply* reply)
 {
-  mwin->insLog("NicoLiveManager::adminAlertFinished");
-  QString repdata = QString(reply->readAll());
+    mwin->insLog("NicoLiveManager::adminAlertFinished");
+    QString repdata = QString(reply->readAll());
 
-  StrAbstractor wakuTcpi(repdata);
+    StrAbstractor wakuTcpi(repdata);
 
-  QString status = wakuTcpi.midStr("status=\"","\"");
-  if (status == "fail") {
-    QString code = wakuTcpi.midStr("<code>","</code>");
-    mwin->insLog(code);
-    QMessageBox::information(mwin, "Viqo",
-                             QStringLiteral(u"放送開始通知の情報取得(AdminAlert)に失敗しました\ncode : ")
-                             + code);
-    return;
-  }
+    QString status = wakuTcpi.midStr("status=\"","\"");
+    if (status == "fail") {
+        QString code = wakuTcpi.midStr("<code>","</code>");
+        mwin->insLog(code);
+        QMessageBox::information(mwin, "Viqo",
+                                 QStringLiteral(u"放送開始通知の情報取得(AdminAlert)に失敗しました\ncode : ")
+                                 + code);
+        return;
+    }
 
-  StrAbstractor* communityi = wakuTcpi.mid("<communities>","</communities>");
-  QString mycommunity;
-  officialMyCommunities.clear();
-  while (!(mycommunity = communityi->midStr("<community_id>","</community_id>")).isNull()) {
-    officialMyCommunities << mycommunity;
-  }
-  updateMyCommunities();
+    StrAbstractor* communityi = wakuTcpi.mid("<communities>","</communities>");
+    if (communityi == nullptr) {
+        mwin->insLog(reply->errorString());
+        QMessageBox::information(mwin, "Viqo",
+                                 QStringLiteral(u"放送開始通知の情報取得(AdminAlert)に失敗しました"));
+        return;
+    }
 
-  waku_addr = wakuTcpi.midStr("<addr>", "</addr>");
-  waku_port = wakuTcpi.midStr("<port>", "</port>").toInt();
-  waku_thread = wakuTcpi.midStr("<thread>", "</thread>");
+    QString mycommunity;
+    officialMyCommunities.clear();
+    while (!(mycommunity = communityi->midStr("<community_id>","</community_id>")).isNull()) {
+        officialMyCommunities << mycommunity;
+    }
+    updateMyCommunities();
 
-  // mwin->insLog("waku addr: " + waku_addr +
-  //              "\nport: " + QString::number(waku_port) +
-  //              "\nthread:" + waku_thread + "\n");
+    waku_addr = wakuTcpi.midStr("<addr>", "</addr>");
+    waku_port = wakuTcpi.midStr("<port>", "</port>").toInt();
+    waku_thread = wakuTcpi.midStr("<thread>", "</thread>");
 
-  if ( wakutcp != nullptr ) {
-    if ( wakutcp->isConnected() )
-      wakutcp->close();
-    wakutcp->deleteLater();
-  }
+    // mwin->insLog("waku addr: " + waku_addr +
+    //              "\nport: " + QString::number(waku_port) +
+    //              "\nthread:" + waku_thread + "\n");
 
-  wakutcp = new WakuTcp(waku_addr, waku_port, waku_thread, mwin, this);
-  wakutcp->doConnect();
+    if ( wakutcp != nullptr ) {
+        if ( wakutcp->isConnected() )
+            wakutcp->close();
+        wakutcp->deleteLater();
+    }
 
-  mwin->insLog();
-  reply->deleteLater();
+    wakutcp = new WakuTcp(waku_addr, waku_port, waku_thread, mwin, this);
+    wakutcp->doConnect();
+
+    mwin->insLog();
+    reply->deleteLater();
 }
 
 void NicoLiveManager::alertReconnect()
 {
-  QString mail = mwin->settings.getUserMail();
-  QString pass = mwin->settings.getUserPass();
+    QString mail = mwin->settings.getUserMail();
+    QString pass = mwin->settings.getUserPass();
 
-  if ( mail.isEmpty() || pass.isEmpty() ) {
-    mwin->insLog("mail or pass are not specified");
-    QMessageBox::information(mwin, "Viqo",
-                             QStringLiteral(u"メールまたはパスワードが設定されていません"));
-    return;
-  }
-  loginAlertAPI(mail, pass);
-  getRawMyLiveHTML();
-  QTimer::singleShot(30000, this, SLOT(getRawMyLiveHTML()));
+    if ( mail.isEmpty() || pass.isEmpty() ) {
+        mwin->insLog("mail or pass are not specified");
+        QMessageBox::information(mwin, "Viqo",
+                                 QStringLiteral(u"メールまたはパスワードが設定されていません"));
+        return;
+    }
+    loginAlertAPI(mail, pass);
+    getRawMyLiveHTML();
+    QTimer::singleShot(30000, this, SLOT(getRawMyLiveHTML()));
 }
 
 
 void NicoLiveManager::updateMyCommunities()
 {
-  mycommunities.clear();
-  mycommunities.append(officialMyCommunities);
+    mycommunities.clear();
+    mycommunities.append(officialMyCommunities);
 }
